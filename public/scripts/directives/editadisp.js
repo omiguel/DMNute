75e092e8722e09761f88df800b4d2edf828e54b8
osvaldo <osvaldo.miguel@grad.ufsc.br>
2016-02-15 11:16:05 -0200

    mandando atualizado para o server

--- public/scripts/directives/editadisp.js
+++ public/scripts/directives/editadisp.js
@@ -11,11 +11,14 @@ app.directive('editadisp', function(){
             var me = this;
             me.listeners = {};
             scope.editDisp = {};
-            scope.mapaatual = {};
+            scope.mapamodel = {};
+            scope.situacaomodel = {};
             scope.mapas = [];
+            scope.situacoes = [];
 
             me.setDisp = function(disp){
                 scope.editDisp = disp;
+                scope.situacaomodel.atual = disp.situacao;
                 var atual = {
                     nome: 'Selecione um mapa',
                     img: '/image/mapa/semmapa.JPG'
@@ -24,23 +27,47 @@ app.directive('editadisp', function(){
                     atual.nome = disp.mapa.nome;
                     atual.img = disp.mapa.img;
                 }
-                scope.mapaatual.atual = atual;
-                console.log('que porra é essa?', scope.mapaatual);
+                scope.mapamodel.atual = atual;
             };
 
             me.recebeMapas = function(mapas){
                 scope.mapas = mapas;
-                console.log('será que esta dando pau aqui?', mapas);
             };
 
             scope.trocaMapa = function(){
-                console.log('trocando aqui', scope.mapaatual);
-                scope.mapaatual.atual = JSON.parse(scope.mapaatual.novo);
+                scope.mapamodel.atual = JSON.parse(scope.mapamodel.novo);
+            };
+
+            me.recebeSituacao = function(msg){
+                scope.situacoes = msg.getDado();
+            };
+
+            scope.salvarEdicao = function(){
+                if(scope.mapamodel.novo){
+                    scope.editDisp.mapa = JSON.parse(scope.mapamodel.novo);
+                }
+                if(scope.situacaomodel.novo){
+                    scope.editDisp.situacao = JSON.parse(scope.situacaomodel.novo);
+                }
+                var dispEditado = new Mensagem(me, 'dispositivo.update', scope.editDisp, 'dispositivo');
+                SIOM.emitirServer(dispEditado);
+            };
+
+            me.editou = function(msg){
+                //todo, avisar ao usuario que as alteracoes foram salvas com sucesso.
+                scope.limpaModels();
+            };
+
+            scope.limpaModels = function(){
+                delete scope.mapamodel.novo;
+                delete scope.situacaomodel.novo;
             };
 
             me.wiring = function(){
                 me.listeners['editadisp'] = me.setDisp.bind(me);
                 me.listeners['mapasprontos'] = me.recebeMapas.bind(me);
+                me.listeners['situacao.readed'] = me.recebeSituacao.bind(me);
+                me.listeners['dispositivo.updated'] = me.editou.bind(me);
 
                 for(var name in me.listeners){
                     SIOM.on(name, me.listeners[name]);
