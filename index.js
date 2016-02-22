3d898c1b0fba479d9ad477776343d9d8a143d82f
osvaldo <osvaldo.miguel@grad.ufsc.br>
2016-02-22 08:29:59 -0300

    atualizada a porta e adicionadas imagens para testar no server.

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ io.on('connection',function(socket){
     new rtcLogin({socket: socket});
 });
 
-http.listen(80, function(err){
-    console.log("Rodando na porta 80", err);
+http.listen(1337, function(err){
+    console.log("Rodando na porta 1337", err);
 });
 
 app.use('/image', express.static(path.resolve(__dirname + '/image/')));
